9b3f7113cd98077cbf786a0d201e20901c834226
henrique <102448580+henriquelindaocareca@users.noreply.github.com>
2024-04-09 12:31:01 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,52 @@
 const botoes = document.querySelectorAll(".botao");
 
-for(let i=0; i<botoes.length;i++){
+//laço de repetição
+
+for(let i = 0; i < botoes.length; i++){
     botoes[i].onclick = function(){
-        for(let j=0;botoes.length;j++){
+        for (let j = 0; j < botoes.length; j++){
             botoes[j].classList.remove("ativo");
         }
         botoes[i].classList.add("ativo");
     }
 }
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-12-05T12:00:00");
+const tempoObjetivo2 = new Date("2024-12-03T12:00:00");
+const tempoObjetivo3 = new Date("2024-12-30T12:00:00");
+const tempoObjetivo4 = new Date("2024-12-01T12:00:00");
+
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+
+function calcularTempo(tempoObjetivo1){
+let tempoAtual = new Date();
+let tempoFinal = tempoObjetivo1 - tempoAtual;
+let segundos = Math.floor (tempoFinal/1000);
+let minutos = Math.floor (segundos/60);
+let horas = Math.floor (minutos/60);
+let dias = Math.floor (horas/24);
+
+segundos %= 60;
+minutos %=60;
+horas %=24;
+
+return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+}
+
+contadores [0].textContent = calcularTempo(tempoObjetivo1);
+
+function atualizaCronometro(){
+     for(let i=0; i<contadores.length;i++)
+     contadores[i].textContent = calcularTempo(tempos[i]);
+}
+
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro,1000);
+}
+
+comecaCronometro();
